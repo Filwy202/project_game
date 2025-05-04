d7f3d4fef5691b9d2498b32ea0dda686df5baa88
Filwy202 <nguyenvietnhuong22@gmail.com>
2025-05-04 23:07:34 +0700

    Add files via upload

---
+++ Project_Game_Pacman/GhostManager.cpp
@@ -0,0 +1,93 @@
+﻿#include <array>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
+#include "Headers/Global.hpp"
+#include "Headers/Pacman.hpp"
+#include "Headers/Ghost.hpp"
+#include "Headers/GhostManager.hpp"
+
+GhostManager::GhostManager() :
+	current_wave(0),
+	wave_timer(LONG_SCATTER_DURATION),
+	ghosts({Ghost(0), Ghost(1), Ghost(2), Ghost(3)})
+{
+
+}
+
+void GhostManager::draw(bool i_flash, sf::RenderWindow& i_window)
+{
+	// Lặp qua từng bóng ma và vẽ nó
+	for (Ghost& ghost : ghosts)
+	{
+		ghost.draw(i_flash, i_window);
+	}
+}
+
+// Hàm reset trạng thái của bóng ma khi bắt đầu màn chơi mới hoặc sau khi reset
+void GhostManager::reset(unsigned char i_level, const std::array<Position, 4>& i_ghost_positions)
+{
+	current_wave = 0;
+
+	// Tính lại thời gian wave dựa trên level
+	wave_timer = static_cast<unsigned short>(LONG_SCATTER_DURATION / pow(2, i_level));
+
+	// Cập nhật lại vị trí ban đầu của các bóng ma
+	for (unsigned char a = 0; a < 4; a++)
+	{
+		ghosts[a].set_position(i_ghost_positions[a].x, i_ghost_positions[a].y);
+	}
+
+	// Reset lại trạng thái của các bóng ma (có thể reset các giá trị trạng thái như hướng đi, chế độ...)
+	for (Ghost& ghost : ghosts)
+	{
+		// Dùng vị trí của ghost[2] và ghost[0] để reset lại các ghost (có thể là để căn chỉnh lại hướng đi)
+		ghost.reset(ghosts[2].get_position(), ghosts[0].get_position());
+	}
+}
+
+// Hàm cập nhật trạng thái của các bóng ma
+void GhostManager::update(unsigned char i_level, std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map, Pacman& i_pacman)
+{
+	// Nếu Pac-Man không còn hiệu lực của energizer
+	if (0 == i_pacman.get_energizer_timer()) 
+	{
+		// Nếu thời gian wave đã hết, chuyển sang sóng bóng ma tiếp theo
+		if (0 == wave_timer)
+		{
+			if (7 > current_wave)
+			{
+				current_wave++;
+
+				for (Ghost& ghost : ghosts)
+				{
+					ghost.switch_mode();
+				}
+			}
+
+			
+			if (1 == current_wave % 2)
+			{
+				wave_timer = CHASE_DURATION;
+			}
+			else if (2 == current_wave)
+			{
+				wave_timer = static_cast<unsigned short>(LONG_SCATTER_DURATION / pow(2, i_level));
+			}
+			else
+			{
+				wave_timer = static_cast<unsigned short>(SHORT_SCATTER_DURATION / pow(2, i_level));
+			}
+		}
+		else
+		{
+			// Nếu chưa hết thời gian của wave, giảm thời gian wave xuống
+			wave_timer--;
+		}
+	}
+
+	for (Ghost& ghost : ghosts)
+	{
+		ghost.update(i_level, i_map, ghosts[0], i_pacman);
+	}
+}
---
+++ Project_Game_Pacman/MapCollision.cpp
@@ -0,0 +1,91 @@
+﻿#include <array>
+#include <cmath>
+
+#include "Headers/Global.hpp"
+#include "Headers/MapCollision.hpp"
+
+// Hàm kiểm tra va chạm và xử lý các vật thể
+bool map_collision(bool i_collect_pellets, bool i_use_door, short i_x, short i_y, std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map)
+{
+	bool output = 0;
+
+	// Chuyển đổi tọa độ pixel thành tọa độ ô trên bản đồ
+	float cell_x = i_x / static_cast<float>(CELL_SIZE);
+	float cell_y = i_y / static_cast<float>(CELL_SIZE);
+
+	// Kiểm tra va chạm với các ô trong bản đồ (4 góc)
+	for (unsigned char a = 0; a < 4; a++)
+	{
+		short x = 0;
+		short y = 0;
+
+		// Tính toán tọa độ ô dựa trên tọa độ pixel
+		switch (a)
+		{
+			case 0: 
+			{
+				x = static_cast<short>(floor(cell_x));
+				y = static_cast<short>(floor(cell_y));
+
+				break;
+			}
+			case 1: 
+			{
+				x = static_cast<short>(ceil(cell_x));
+				y = static_cast<short>(floor(cell_y));
+
+				break;
+			}
+			case 2: 
+			{
+				x = static_cast<short>(floor(cell_x));
+				y = static_cast<short>(ceil(cell_y));
+
+				break;
+			}
+			case 3: 
+			{
+				x = static_cast<short>(ceil(cell_x));
+				y = static_cast<short>(ceil(cell_y));
+			}
+		}
+
+		// Kiểm tra nếu tọa độ nằm trong phạm vi bản đồ
+		if (0 <= x && 0 <= y && MAP_HEIGHT > y && MAP_WIDTH > x)
+		{
+			if (0 == i_collect_pellets)
+			{
+				// Kiểm tra nếu va chạm với tường
+				if (Cell::Wall == i_map[x][y])
+				{
+					output = 1;
+				}
+				// Kiểm tra nếu không sử dụng cửa và va chạm với cửa
+				else if (0 == i_use_door && Cell::Door == i_map[x][y])
+				{
+					output = 1;
+				}
+			}
+			else 
+			{
+
+				// Kiểm tra nếu va chạm với energizer
+				if (Cell::Energizer == i_map[x][y])
+				{
+					output = 1;
+
+					// Đặt ô đó thành ô trống sau khi thu thập energize
+					i_map[x][y] = Cell::Empty;
+				}
+				
+				else if (Cell::Pellet == i_map[x][y])
+				{
+					// Đặt ô đó thành ô trống sau khi thu thập pellet
+					i_map[x][y] = Cell::Empty;
+				}
+			}
+		}
+	}
+
+	return output;
+}
